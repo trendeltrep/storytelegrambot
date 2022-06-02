e0652753bc37f1ce37db017826ba0a3db132b144
trendeltrep <maksym.krykukha@nure.ua>
2022-06-02 22:09:49 +0300

    02062209

--- testBOT.py
+++ testBOT.py
@@ -31,8 +31,13 @@ def send_welcome(message):
 @bot.message_handler(commands='request')
 def request_bot(message):
 	text = "привет"
+	
+	
 	r = requests.get(f"https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key={api_key}&lang=ru-en&text={text}")
-	bot.send_message(chat_id=message.chat.id,text=f"{r.text}")
+
+	answer = r["def"][0]["pos"]
+
+	bot.send_message(chat_id=message.chat.id,text=f"{answer}")
 
 @bot.message_handler(commands='help')
 def help_bot(message):
