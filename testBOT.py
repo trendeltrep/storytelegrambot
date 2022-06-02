f59082167292b663dc7958015ec6d20b6741e715
trendeltrep <maksym.krykukha@nure.ua>
2022-06-02 21:54:27 +0300

    02062154 yandex api first

--- testBOT.py
+++ testBOT.py
@@ -1,10 +1,14 @@
+from cgitb import text
 import telebot
 from telebot import types
 import io
 import random
+import requests
 
 
 TOKEN = "5399350051:AAEemaHyed5a_BwaDyHqAnn3YTy1L0nwrPM"
+
+api_key = "dict.1.1.20220602T154853Z.31339893ad01778b.36cf74dd17c8a6023f59cfbb0bfe7d6d8e41f6a7"
 
 words = []
 helpwords = []
@@ -13,18 +17,22 @@ bot = telebot.TeleBot(TOKEN,parse_mode=None)
 
 @bot.message_handler(commands='start')
 def send_welcome(message):
-	bot.reply_to(message, "You've started the bot")
 	markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
 	
 	itembthelp = types.KeyboardButton('/help')
 	
 	markup.row(itembthelp)
 	
-
 # @bot.message_handler(commands='stop')
 # def stop_bot(message):
 # 	bot.reply_to(message,"You've stopped the bot")
 # 	bot.stop_bot()
+
+@bot.message_handler(commands='request')
+def request_bot(message):
+	text = "привет"
+	r = requests.get(f"https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key={api_key}&lang=ru-en&text={text}")
+	bot.send_message(chat_id=message.chat.id,text=f"{r.text}")
 
 @bot.message_handler(commands='help')
 def help_bot(message):
@@ -33,7 +41,6 @@ def help_bot(message):
 /add - Add list of words (which were read) to database (Automatically add when length is over 512 words) 
 /count - Show count of words in database
 /database - Sends a db.txt with all words in db''')
-
 
 @bot.message_handler(commands='count')
 def count_word_bot(message):
@@ -53,20 +60,14 @@ def count_word_bot(message):
 
 @bot.message_handler(commands="add")
 def add_db_bot(message):
-	
-
 	with io.open('db.txt','r', encoding="utf-8") as f:
-		lines = f.readlines()
-		
+		lines = f.readlines()	
 		allLinesInText = str(lines).__str__().replace('[','').replace(']','').replace(',','').replace('"','').replace("'",'')
 		allLinesInText.replace("\r"," ").replace("\n"," ")
 		allLinesInText.strip()
 		newWords = allLinesInText.split(" ")
-
 		helpNewWords = str(newWords).__str__().replace('[','').replace(']','').replace(',','').replace('"','').replace("'",'')
-
 		finalText=f'{helpNewWords} '
-
 		for i in range(0,len(words)):
 			if words[i] not in newWords:
 				finalText+= f"{words[i]} "
@@ -83,7 +84,6 @@ def db_bot(message):
 	with io.open("db.txt","r",encoding="utf-8") as file:
 		length = file.readline()
 		file.close()
-
 	with open("db.txt", "rb") as file:
 		if (len(length)==0):
 			bot.send_message(chat_id=message.chat.id,text="База слов пустая")
@@ -93,16 +93,10 @@ def db_bot(message):
 			bot.send_message(chat_id=message.chat.id,text="База слов успешно скинута")
 			file.close()
 	
-	
-	
-
 
 
 @bot.message_handler(commands='clearBASErnBTWjustDOitMC')
 def clear_db(message):
-
-	
-
 	with io.open('db.txt','a') as f:
 		with io.open('db.txt','w') as f:
 			f.write(f'')
@@ -116,13 +110,11 @@ def story_bot(message):
 	story = ""
 	with io.open('db.txt','r',encoding="utf-8") as f:
 		lines = f.readlines()
-		
 		allLinesInText = str(lines).__str__().replace('[','').replace(']','').replace(',','').replace('"','').replace("'",'')
 		allLinesInText.replace("\r"," ").replace("\n"," ")
 		allLinesInText.strip()
 		newWords = allLinesInText.split(" ")
-		count = len(newWords)
-		
+		count = len(newWords)		
 		if count <= minNum:
 			bot.send_message(chat_id=message.chat.id,text=f'База слов достаточно мала ({count} < {minNum+1})')
 		else:
@@ -153,21 +145,15 @@ def echo_all(message):
 		for i in range (0,len(arr)):
 			if(arr[i] not in words and len(arr[i])<15):
 				words.append(arr[i])	
-
-
 		if len(words) >=200:
 				with io.open('db.txt','r', encoding="utf-8") as f:
-					lines = f.readlines()
-					
+					lines = f.readlines()	
 					allLinesInText = str(lines).__str__().replace('[','').replace(']','').replace(',','').replace('"','').replace("'",'')
 					allLinesInText.replace("\r"," ").replace("\n"," ")
 					allLinesInText.strip()
 					newWords = allLinesInText.split(" ")
-
 					helpNewWords = str(newWords).__str__().replace('[','').replace(']','').replace(',','').replace('"','').replace("'",'')
-
 					finalText=f'{helpNewWords} '
-
 					for i in range(0,len(words)):
 						if words[i] not in newWords:
 							finalText+= f"{words[i]} "
